4cbb85741135c8f9bade85db7418f39444dc2306
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-02-01 13:32:13 +0530

    feat: add handlebars

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const Gig = require('../models/Gig');
+const {Gig} = require('../models/Gig');
 
 router.get('/', async (req, res) => {
     try {
-        const gigs = await Gig.findAll()
+        const gigs = await Gig.findAll();
         console.log(gigs);
+        res.sendStatus(200);
     }
     catch(err)
     {
         console.log(err.message);
     }
-})
+});
+
+router.get('/add', async (req, res) => {
+    const data = {
+        title:'React Native Web App',
+        technologies: 'React Native,React.js,Node.js,Express.js,MongoDB,Mongoose',
+        budget:'$2000',
+        description:'',
+        contact_email:'user@example.com'
+    };
+
+    let {title, technologies, budget, description, contact_email} = data;
+
+    try{
+        await Gig.create({title, technologies, budget, description, contact_email});
+        res.redirect('/gigs');
+    }catch(err){
+        console.log(err.message);
+    }
+});
 
 module.exports = router;
